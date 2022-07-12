dd6439574f76fa3b9bb0e1bb23b072006e3ac049
isaac gomes <93602614+isaac545454@users.noreply.github.com>
2022-07-12 16:38:25 -0300

    Update header.php

--- template/header.php
+++ template/header.php
@@ -8,6 +8,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+           <meta http-equiv='Content-Security-Policy' content='upgrade-insecure-requests'>
     <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
