5defee6b3cbffe5c16368622a80a31d38d6e6f07
isaac gomes <93602614+isaac545454@users.noreply.github.com>
2022-07-12 15:08:22 -0300

    Update header.php

--- template/header.php
+++ template/header.php
@@ -8,7 +8,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= $BASE_URL ?>./css/style.css">
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Montserrat:wght@300;700&family=Pacifico&display=swap" rel="stylesheet">
